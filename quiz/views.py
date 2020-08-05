7b6b49626e2d755503e7dabbbce11211c488d758
Diego Gomez <diegogomez268@gmail.com>
2020-08-05 15:31:12 -0500

    fixing how data is shown into template

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 import requests
 from django.views.decorators.csrf import csrf_protect
+import json
+
+from .models import Quiz
 # Create your views here.
 url = "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean"
 
@@ -9,6 +12,14 @@ def quiz_app(request):
     '''quiz api'''
     response = requests.get(url)
     data = response.json()
-    return render(request, 'test/quiz.html', {
-        'results': data['results'],
+    quiz = Quiz.objects.all()
+    if quiz:
+        print(quiz[0].data['results'])
+        return render(request, 'test/quiz.html', {
+        'results': quiz[0].data['results'],
     })
+    else:
+        quiz = Quiz.objects.create(data=data)
+        return render(request, 'test/quiz.html', {
+        'results': quiz[0].data['results'],
+        })
