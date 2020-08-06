8909a57a65012bfdcd7cffee10be2c64ef87cc0c
Diego Gomez <diegogomez268@gmail.com>
2020-08-05 20:00:01 -0500

    added passing through questions and getting answers and results

--- quiz/views.py
+++ quiz/views.py
@@ -15,24 +15,43 @@ def quiz_app(request):
     data = response.json()
     quiz = Quiz.objects.all()
     count = 1
+    final_result = 0
+    response = ''
     if quiz:
-        print(quiz[0].data['results'])
         return render(request, 'test/quiz.html', {
         'results': quiz[0].data['results'],
-        'count':count})
+        'count':count,
+        'final_result':final_result,
+        'response':response})
     else:
         quiz = Quiz.objects.create(data=data)
         return render(request, 'test/quiz.html', {
         'results': quiz[0].data['results'],
-        'count':count})
+        'count':count,
+        'final_result':final_result,})
 
 def question(request):
     '''advance questions'''
     quiz = Quiz.objects.all()
     count = int(request.POST.get('counter'))
+    final_result = int(request.POST.get('final_result_data'))
+    answer_data = request.POST.get('answer')
+    answers = quiz[0].data['results'][count-1]
+    response = str(request.POST.get('response_data'))
+    if answers['correct_answer'] == answer_data:
+        final_result = final_result+1
+        response =  response + answers['question']  + ' CORRECT!! '
+    else:
+        response =  response + answers['question'] + ' INCORRECT!!'
     count = count+1
     if count > 10:
-        return render(request,'test/result.html')
+        responses = response.split("!!")
+        print(responses)
+        return render(request,'test/results.html',{'final_result':final_result, 'responses':responses})
+    print(final_result)
     return render(request, 'test/quiz.html', {
         'results': quiz[0].data['results'],
-        'count':count})
+        'count':count,
+        'final_result':final_result,
+        'response':response})
+
